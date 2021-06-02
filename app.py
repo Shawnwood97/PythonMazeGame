8a24c0b03a0acf347258ad5ced4c068d073fcb17
Shawn Wood <Shawnwood97@gmail.com>
2021-06-01 20:49:01 -0600

    board prints, player prints, movement conditional created!

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import gameboard
-import player
+from gameboard import GameBoard
+from player import Player
 
 print("Welcome to the game!")
 print("Instructions: ")
@@ -15,9 +15,26 @@ print("-----------------------------")
 # Create a new GameBoard called board
 # Create a new Player called played starting at position 3,2
 
+# Set board to GameBoard
+board = GameBoard()
+
+# Set player to Player, passing args 3 and 2 for position on row/column
+player = Player(3, 2)
+
 while True:
     board.printBoard(player.rowPosition, player.columnPosition)
     selection = input("Make a move: ")
     # TODO
-    # Move the player through the board
+    # Move the player through the boardW
     # Check if the player has won, if so print a message and break the loop!
+
+    if(selection == 'w'):
+        player.moveUp()
+    elif(selection == 'a'):
+        player.moveLeft()
+    elif(selection == 's'):
+        player.moveDown()
+    elif(selection == 'd'):
+        player.moveRight()
+    else:
+        print('Error: Please enter a valid movement command!')
--- player.py
+++ player.py
@@ -4,13 +4,16 @@ class Player:
         self.columnPosition = initialColumn
 
     # TODO
-    # def moveUp(self):
+    def moveUp(self):
+        self.rowPosition = self.rowPosition - 1
+    # TODO
+    def moveDown(self):
+        self.rowPosition = self.rowPosition + 1
 
     # TODO
-    # def moveDown(self):
+    def moveLeft(self):
+        self.columnPosition = self.columnPosition - 1
 
     # TODO
-    # def moveLeft(self):
-
-    # TODO
-    # def moveRight(self):
+    def moveRight(self):
+        self.columnPosition = self.columnPosition + 1
