bd02331d356585078f92cc257e23cd5c3a2913cf
Shawn Wood <Shawnwood97@gmail.com>
2021-06-03 15:45:53 -0600

    collect coins working, poggy

--- gameboard.py
+++ gameboard.py
@@ -8,6 +8,7 @@ class GameBoard:
     # self.coinList = [Coins(1,1)]
     self.coinRow = [4, 13, 19, 7, 9] #13, 19, 7, 9
     self.coinColumn = [16, 11, 4, 3, 13] #
+    self.coinsCollected = 0
     # Thought you could confuse me with your list formatting, huh? :)
     self.board = [
       ["🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫"],
@@ -33,11 +34,23 @@ class GameBoard:
       ["🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫", "🟫"],
     ] 
   
-  def getCoins(self):
+  def addCoins(self):
     counter = 0
     while counter < len(self.coinRow):
       self.board[self.coinRow[counter]][self.coinColumn[counter]] = '🟨'
       counter = counter + 1
+
+  def collectCoin(self, playerRow, playerColumn):
+    counter = 0
+    while counter < len(self.coinRow):
+      if(playerRow == self.coinRow[counter] and playerColumn == self.coinColumn[counter]):
+        print('Coin Collected')
+        self.coinRow.pop(counter)
+        self.coinColumn.pop(counter)
+        break
+      else:
+        counter = counter + 1
+        # self.board[playerRow][playerColumn] = '⬛'
     
   def printBoard(self, playerRow, playerColumn):
     for i in range(len(self.board)):
@@ -47,18 +60,22 @@ class GameBoard:
         else:
           print(self.board[i][j], end="")
       print("")
-      self.getCoins()
-      # counter = 0
-      # while counter < len(self.coinRow):
-      #   self.board[self.coinRow[counter]][self.coinColumn[counter]] = '🟨'
-      #   counter = counter + 1
+      self.addCoins()
+    counter = 0
+    while counter < len(self.coinRow):
+      if(playerRow == self.coinRow[counter] and playerColumn == self.coinColumn[counter]):
+        self.board[playerRow][playerColumn] = '⬛'
+        self.coinRow.pop(counter)
+        self.coinColumn.pop(counter)
+        self.coinsCollected += 1
+        print(f'Coin Collected ---- Total Collected: {self.coinsCollected}')
+        break
+      else:
+        counter = counter + 1
+      # self.collectCoin(playerRow, playerColumn)
 
     # changed the return False to a raised exception to avoid double error from movement else block in app.py
   def checkMove(self, testRow, testColumn):
-      # if(self.board[testRow][testColumn].find("🟨") != -1):
-      #     self.board[testRow][testColumn] = ("⬛")
-      #     print('Coin Collected')
-      # self.board[testRow][testColumn].remove("")
     if(self.board[testRow][testColumn].find("🟫") != -1):
       raise NoWallRununing
     if(self.board[testRow][testColumn].find("🟦") != -1):
